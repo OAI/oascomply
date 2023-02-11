e5560f274d689316c502769163364f9f4a56a924
Henry H. Andrews <andrews_henry@yahoo.com>
2023-02-10 22:10:15 -0800

    Add oastypes vocab plugin for jschon
    This belongs in its own package but it's fine here for now.

---
+++ oasparse/oastype4jschon.py
@@ -0,0 +1,8 @@
+from jschon.vocabulary.annotation import AnnotationKeyword
+
+class OasType(AnnotationKeyword):
+    key = 'oasType'
+
+class OasSubType(AnnotationKeyword):
+    key = 'oasSubType'
+
