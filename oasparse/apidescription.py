23674c0426c91ccac5b629341e1f50f746318ebf
Henry H. Andrews <andrews_henry@yahoo.com>
2023-05-08 16:55:39 -0700

    Better argument support, base URI calculation
    Plus more help text, imports that emphasize namespacing more,
    and other minor improvements.

--- oasparse/apidescription.py
+++ oasparse/apidescription.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 from pathlib import Path
+import urllib
 from uuid import uuid4
 from collections import namedtuple
 from typing import Any, Mapping, Optional, Tuple
@@ -14,6 +15,7 @@ import rfc3987
 import yaml
 import json_source_map as jmap
 import yaml_source_map as ymap
+from yaml_source_map.errors import InvalidYamlError
 
 from oasparse.oasgraph import OasGraph
 from oasparse.schemaparse import Annotation, SchemaParser
@@ -25,8 +27,46 @@ __all__ = [
 logger = logging.getLogger(__name__)
 
 
-UriPrefix = namedtuple('UriPrefix', ['prefix', 'dir'])
+OUTPUT_FORMATS_LINE = frozenset({
+    'nt11',                     # N-Triples UTF-8 encoded (default)
+    'nt',                       # N-Triples
+    'ntriples',                 # N-Triples
+    'application/n-triples',    # N-Triples
+    'nquads',                   # N-Quads
+    'applicaation/n-quads',     # N-Quads
+})
+OUTPUT_FORMATS_STRUCTURED = frozenset({
+    'ttl',                      # Turtle
+    'turtle',                   # Turtle
+    'text/turtle',              # Turtle
+    'longturtle',               # Turtle with more space
+    'ttl2',                     # Turtle with more space
+    'n3',                       # Notation-3
+    'text/n3',                  # Notation-3
+    'json-ld',                  # JSON-LD
+    'application/ld+json',      # JSON-LD
+    'xml',                      # RDF/XML
+    'application/rdf+xml',      # RDF/XML
+    'pretty-xml',               # RDF/XML (prettier)
+    'trig',                     # Trig (Turtle for quads)
+    'application/trig',         # Trig (Turtle for quads)
+    'trix',                     # Trix (RDF/XML for quads)
+    'application/trix',         # Trix (RDF/XML for quads)
+})
 
+HELP_EPILOG = """
+See the README for further information on:
+
+* How API description data appears in the output
+* How to extract human-friendly names from the output
+* API description document URLs vs URIs
+* Handling multi-document API descriptions
+* Handling complex referencing scenarios
+* What you need to know about IRIs vs URIs/URLs
+"""
+
+
+UriPrefix = namedtuple('UriPrefix', ['dir', 'prefix'])
 
 class ApiDescription:
     """
@@ -58,9 +98,19 @@ class ApiDescription:
                 raise NotImplementedError("OAS v3.1 support stil in progress")
             raise ValueError(f"OAS v{self._version} not supported!")
 
+        base_uri = rfc3987.parse(uri, rule='IRI')
+        if base_uri['path'] and not base_uri['path'].endswith('/'):
+            # RDF serialization works better with a directory
+            # as a base IRI, particularly for multi-document
+            # API descriptions within a single directory.
+            # Otherwise it fails to notice many opportunities to
+            # shorten IRI-references.
+            base_uri['path'] = (
+                base_uri['path'][:base_uri['path'].rindex('/') + 1]
+            )
         self._g = OasGraph(
             self._version[:self._version.rindex('.')],
-            base=uri,
+            base=rfc3987.compose(**base_uri),
         )
         self._primary_uri = uri
 
@@ -104,11 +154,13 @@ class ApiDescription:
             try:
                 data = self._contents[uri]
                 return (
-                    JSONPointer.parse_uri_fragment(fragment).evaluate(data),
+                    jschon.JSONPointer.parse_uri_fragment(
+                        fragment
+                    ).evaluate(data),
                     self._sources.get(uri),
                 )
 
-            except (KeyError, JSONPointerError):
+            except (KeyError, jschon.JSONPointerError):
                 return None, None
 
     def validate(self, resource_uri=None, oastype='OpenAPI'):
@@ -121,7 +173,6 @@ class ApiDescription:
             resource_uri = self._primary_uri
 
         data, sourcemap = self.get(resource_uri)
-        logger.error('\n'.join(sourcemap.keys()))
         assert data is not None
 
         output = sp.parse(data, oastype)
@@ -144,31 +195,42 @@ class ApiDescription:
                 self.validate(uri, oastype)
 
     @classmethod
-    def _process_resource_arg(cls, r, prefixes):
-        if isinstance(r, str):
-            path = Path(r)
-            full_path = path.resolve()
-            uri = full_path.with_suffix('').as_uri()
-            for p in prefixes:
-                try:
-                    rel = full_path.relative_to(p.dir)
-                    uri = p.prefix + str(rel.with_suffix(''))
-                except ValueError:
-                    pass
-        else:
-            path = Path(r[0])
+    def _process_resource_arg(cls, r, prefixes, create_source_map):
+        path = Path(r[0])
+        full_path = path.resolve()
+        if len(r) > 1:
+            # TODO: Support semantic type
             uri = r[1]
+        else:
+            uri = full_path.with_suffix('').as_uri()
+        for p in prefixes:
+            try:
+                rel = full_path.relative_to(p.dir)
+                uri = p.prefix + str(rel.with_suffix(''))
+            except ValueError:
+                pass
         filetype = path.suffix[1:] or 'yaml'
         if filetype == 'yml':
             filetype = 'yaml'
 
         content = path.read_text()
+        sourcemap = None
         if filetype == 'json':
             data = json.loads(content)
-            sourcemap = jmap.calculate(content)
+            if create_source_map:
+                sourcemap = jmap.calculate(content)
         elif filetype == 'yaml':
             data = yaml.safe_load(content)
-            sourcemap = ymap.calculate(content)
+            if create_source_map:
+                # The YAML source mapper gets confused sometimes,
+                # just log a warning and work without the map.
+                try:
+                    sourcemap = ymap.calculate(content)
+                except InvalidYamlError:
+                    logger.warn(
+                        f"Unable to calculate source map for {path}",
+                    )
+                    pass
         else:
             raise ValueError(f"Unsupported file type {filetype!r}")
 
@@ -182,7 +244,7 @@ class ApiDescription:
     @classmethod
     def _process_prefix(cls, p):
         try:
-            parsed = rfc3987.parse(p[0], rule='IRI')
+            parsed = rfc3987.parse(p[0], rule='URI')
             if parsed['scheme'] == 'file':
                 raise ValueError(
                     f"'file:' URIs cannot be used as URI prefixes: <{p[0]}>"
@@ -208,7 +270,7 @@ class ApiDescription:
 
         except ValueError:
             try:
-                rfc3987.parse(p[0], rule='IRI_reference')
+                rfc3987.parse(p[0], rule='URI_reference')
                 raise ValueError(f'URI prefixes cannot be relative: <{p[0]}>')
             except ValueError:
                 raise ValueError(
@@ -217,37 +279,116 @@ class ApiDescription:
 
     @classmethod
     def load(cls):
-        parser = argparse.ArgumentParser()
-        parser.add_argument(
-            '-o',
-            '--oas-file',
-            action='append',
-            dest='resources',
-            help="An API description file as a local file path, which will"
-                 "appear in output as the corresponding 'file:' URL",
+        class CustomArgumentParser(argparse.ArgumentParser):
+            def _fix_message(self, message):
+                return message.replace(
+                    'FILES [FILES ...]',
+                    'FILE [URI] [TYPE]',
+                ).replace(
+                    'DIRECTORIES [DIRECTORIES ...]',
+                    'DIRECTORY [URI_PREFIX]',
+                )
+
+            def format_usage(self):
+                # return super().format_usage()
+                return self._fix_message(super().format_usage())
+
+            def format_help(self):
+                # return super().format_help()
+                return self._fix_message(super().format_help())
+
+        parser = CustomArgumentParser(
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            epilog=HELP_EPILOG,
         )
         parser.add_argument(
-            '-O',
-            '--aliased-oas-file',
-            nargs=2,
+            '-f',
+            '--file',
+            nargs='+',
             action='append',
-            dest='resources',
-            help="An API description file path followed by the URI used "
-                 "to identify it in references and output",
+            dest='files',
+            help="An API description file, given as a mandatory file path, "
+                 "optionally followed by an URI to associate with the file, "
+                 "and/or the semantic type of the file's contents, e.g. "
+                 "3.1:Schema; the path MUST be first, while the URI and "
+                 "semantic type can appear in either order; by default, "
+                 "a 'file:' URI will be generated matching the path, "
+                 "and the code will attempt to infer the semantic type "
+                 "from context and reference usage.\n\n"
+                 "See below for scenarios where the semantic type is required."
         )
         parser.add_argument(
             '-p',
             '--uri-prefix',
+            '--iri-prefix',
             nargs=2,
+            metavar=('DIRECTORY', 'URI_PREFIX'),
             action='append',
             dest='prefixes',
-            help="A URI prefix, ending in a '/', followed by a filesystem "
-                 "directory; all paths passed that are not already aliased "
-                 "to a URI that are within the given directory will be "
-                "assigned a URI constructed by replacing the directory with "
-                "the prefix and removing any file extension (e.g. '.yaml' or "
-                "'.json'); Note that 'file:' URIs are not allowed as prefixes "
-                "as the default behavior is to use the appropriate 'file:' URI"
+            help="A directory followed by a URI prefix that MUST have a path "
+                 "ending in '/'; all files loaded from this directory with "
+                 "the '-f' option without specifying a URI will be assigned "
+                 "URIs by replacing the directory with the URI prefix and "
+                 "removing any file extension suffix (e.g. '.json', '.yaml')"
+        )
+        parser.add_argument(
+            '-d',
+            '--directory',
+            nargs='+',
+            # metavar='(see help text)',
+            action='append',
+            dest='directories',
+            help="A directory containing API description files, optionally "
+                 "followed by an URI prefix with a path component ending in "
+                 "a '/';  All files with a .json, .yaml, or .yml anywhere "
+                 "under the directory will be loaded; if an URI prefix is "
+                 "provided, the file path relative to the directory, but "
+                 "without the file extension, will be appended to the prefix "
+                 "to determine each file's URI; otherwise, each file will be "
+                 "assigned its corresponding 'file:' URI.\n\n"
+                 "See below for scenarios where certain files must be "
+                 "loaded separately with -f and -p.",
+        )
+        parser.add_argument(
+            '-i',
+            '--allow-iris',
+            action='store_true',
+            help="Allow IRIs (URIs/URLs with full unicode support) even where "
+                 "OAS and JSON Schema only support URIs/URLs; only use this "
+                 "option if your OAS tooling supports IRIs and you want to "
+                 "suppress errors about using unencoded non-ASCII characters "
+                 "in your URIs/URLs."
+        )
+        parser.add_argument(
+            '-n',
+            '--no-source-map',
+            action='store_true',
+            help="Disable line number tracking for API descriptions, which "
+                 "substantially improves performance; locations wills be "
+                 "reported using JSON Pointers only; Note that currently, the "
+                 "YAML line map package sometimes gets confused and drops "
+                 "the line numbers anyway (this will be fied at some point).",
+        )
+        parser.add_argument(
+            '-o',
+            '--output-format',
+            default='nt11',
+            choices=(
+                'none',                     # Do not write to stdout
+                'hext',                     # Hextuples in NDJSON
+            ),
+            help="Set the format, if any, for writing the graph to stdout. "
+                 "This is passed through to the rdflib python library, but "
+                 "only 'nt11' (N-Triples with UTF-8 encoding) is tested. "
+                 "The default is 'nt11' unless '-t' is passed, in which "
+                 "case the default is 'none' (no output)."
+        )
+        parser.add_argument(
+            '-t',
+            '--store',
+            default='none',
+            choices=(('none',)),
+            help="TODO: Support storing to various kinds of databases."
         )
 
         args = parser.parse_args()
@@ -258,9 +399,11 @@ class ApiDescription:
         # Reverse sort so that the first matching prefix is the longest
         prefixes.sort(reverse=True)
 
-        resources = [
-            cls._process_resource_arg(r, prefixes) for r in args.resources
-        ]
+        resources = [cls._process_resource_arg(
+            r,
+            prefixes,
+            not args.no_source_map,
+        ) for r in args.files]
 
         candidates = list(filter(lambda r: 'openapi' in r['data'], resources))
         if not candidates:
