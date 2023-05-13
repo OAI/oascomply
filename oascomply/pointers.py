ecc6d3a64df72971712c029a4fed2bbd0c338fb9
Henry H. Andrews <andrews_henry@yahoo.com>
2023-05-13 12:26:02 -0700

    [Relative] JSON Pointer templates
    Includes unit tests!
    
    Also, use git repo branch of rfc3986 that avoids warnings.
    rfc3986 2.0.0 issues deprecation warnings when you use non-deprecated
    methods.  See note in pyproject.toml for info on the upstream patch
    and its future release.

---
+++ oascomply/pointers.py
@@ -0,0 +1,253 @@
+from typing import Generator, Sequence, Tuple, Union
+import re
+import jschon
+
+TEMPLATE_UNESCAPED = r'[\x00-.0-z|\x7f-\U0010ffff]'
+TEMPLATE_VARIABLE = f'{TEMPLATE_UNESCAPED}*'
+TEMPLATE_TOKEN = r'\{(' + TEMPLATE_VARIABLE + r')\}'
+TEMPLATE_ESCAPED = r'\~[0123]'
+NON_TEMPLATE_TOKEN = f'(?:{TEMPLATE_UNESCAPED}|{TEMPLATE_ESCAPED})*'
+BASIC_POINTER_TEMPLATE = f'(?:/(?:{TEMPLATE_TOKEN}|{NON_TEMPLATE_TOKEN}))*'
+JSON_POINTER_TEMPLATE = f'{BASIC_POINTER_TEMPLATE}(?:{TEMPLATE_TOKEN}#)?'
+
+
+class GenericPointerTemplateError(Exception):
+    pass
+
+
+class JSONPointerTemplateError(GenericPointerTemplateError):
+    pass
+
+
+class InvalidJSONPointerTemplateError(JSONPointerTemplateError):
+    pass
+
+
+class JSONPointerTemplateEvaluationError(JSONPointerTemplateError):
+    pass
+
+
+class JSONPointerTemplate:
+    def __init__(self, template: str):
+        if (m := re.fullmatch(JSON_POINTER_TEMPLATE, template)) is None:
+            raise InvalidJSONPointerTemplateError(
+                f'{template!r} is not a valid JSONPointerTemplate!'
+            )
+        self._template = template
+
+        # Splitting '' results in [''], and '/' in ['', ''],
+        # so always remove the initial ''
+        segments = template.split('/')[1:]
+
+        # Components are one of:
+        # * JSONPointer instances
+        # * A template variable name (str instance)
+        # * Boolean True to request the name of the key or number
+        #   of the index matching the previous variable; this can
+        #   only occur as the last component
+        self._components= []
+        currptr = jschon.JSONPointer()
+        for s in segments:
+            if s.startswith('{'):
+                if len(currptr) > 0:
+                    self._components.append(currptr)
+                    currptr = jschon.JSONPointer()
+                if s.endswith('#'):
+                    self._components.extend((s[1:-2], True))
+                else:
+                    self._components.append(s[1:-1])
+            else:
+                currptr /= self.unescape(s)
+
+        if len(currptr) or not len(self._components):
+            self._components.append(currptr)
+
+    def __str__(self):
+        return self._template
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, type(self)) and
+            self._template == other._template
+        )
+
+    def evaluate(
+        self,
+        instance: jschon.JSON,
+        _index=0,
+        _resolved=jschon.JSONPointer(),
+        _variables=None,
+        _previous_variable=None,
+    ) -> Generator[
+        Tuple[
+            jschon.JSONPointer,
+            jschon.JSON,
+            Union[None, str, int],
+        ],
+        None,
+        None,
+    ]:
+        remaining = self._components[_index:]
+        variables = _variables or {}
+
+        if len(remaining) == 0:
+            yield _resolved, instance, variables, None
+            return
+
+        next_c = remaining[0]
+        if isinstance(next_c, jschon.JSONPointer):
+            new_resolved = _resolved / next_c
+            try:
+                new_instance = next_c.evaluate(instance)
+            except jschon.JSONPointerError as e:
+                raise JSONPointerTemplateEvaluationError(
+                    f"Path '{new_resolved}' not found in document while"
+                    f"evaluating template '{self}'"
+                ) from e
+
+            yield from self.evaluate(
+                new_instance,
+                _index + 1,
+                new_resolved,
+                variables,
+            )
+
+        elif isinstance(next_c, str):
+            if instance.type == 'array':
+                keys = range(len(instance))
+            elif instance.type == 'object':
+                keys = instance.keys()
+            else:
+                raise JSONPointerTemplateEvaluationError(
+                    f"Cannot match template variable {next_c!r} from "
+                    f"template '{self}' – instance locattion '{_resolved}' "
+                    f"is a {instance.type!r}, not an array or object."
+                )
+
+            for key in keys:
+                newvars = variables.copy()
+                newvars[next_c] = key
+                yield from (
+                    self.evaluate(
+                        instance[key],
+                        _index + 1,
+                        _resolved / str(key),
+                        newvars,
+                    )
+                )
+        else:
+            assert next_c is True
+            prev_val = next(reversed(variables.values()))
+            yield _resolved, instance, variables, prev_val
+
+
+    @staticmethod
+    def escape(component):
+        return (
+            component
+                .replace('~', '~0')
+                .replace('/', '~1')
+                .replace('{', '~2')
+                .replace('}', '~3')
+        )
+
+    @staticmethod
+    def unescape(component):
+        return (
+            component
+                .replace('~3', '}')
+                .replace('~2', '{')
+                .replace('~1', '/')
+                .replace('~0', '~')
+        )
+
+
+class RelativeJSONPointerTemplateError(GenericPointerTemplateError):
+    pass
+
+
+class InvalidRelativeJSONPointerTemplateError(
+    RelativeJSONPointerTemplateError
+):
+    pass
+
+
+class RelativeJSONPointerTemplateEvaluationError(
+    RelativeJSONPointerTemplateError
+):
+    pass
+
+
+class RelativeJSONPointerTemplate:
+    def __init__(self, template: str):
+        try:
+            try:
+                slash = template.index('/')
+                if template[slash -1] == '#':
+                    raise InvalidRelativeJSONPointerTemplateError(
+                        "Can't use '#' in origin adjustment with template path"
+                    )
+                self._relptr = jschon.RelativeJSONPointer(template[:slash])
+                self._jptemplate = JSONPointerTemplate(template[slash:])
+            except ValueError:
+                self._relptr = jschon.RelativeJSONPointer(template)
+                self._jptemplate = None
+        except (
+            jschon.JSONPointerError,
+            jschon.RelativeJSONPointerError,
+            InvalidJSONPointerTemplateError,
+        ) as e:
+            raise InvalidRelativeJSONPointerTemplateError(
+                f"{template!r} is not a valid RelativeJSONPointerTemplate!",
+            ) from e
+        self._template = template
+
+    def __str__(self):
+        return self._template
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, type(self)) and
+            self._template == other._template
+        )
+
+    def evaluate(self, instance):
+        try:
+            base = self._relptr.evaluate(instance)
+        except jschon.RelativeJSONPointerError as e:
+            raise RelativeJSONPointerTemplateEvaluationError(
+                f"Could not evaluate origin adjustment of {self._template}",
+            ) from e
+
+        if self._jptemplate is None:
+            if self._relptr.index:
+                # Since we already evaluated the full relptr,
+                # we know this will not raise an exception
+                value = jschon.RelativeJSONPointer(
+                    self._template[:-1]
+                ).evaluate(instance)
+                name = base
+            else:
+                value = base
+                name = None
+            yield self._relptr, value, {}, name
+            return
+
+        try:
+            yield from (
+                (
+                    jschon.RelativeJSONPointer(
+                        up=self._relptr.up,
+                        over=self._relptr.over,
+                        ref=result[0],
+                    ),
+                    result[1],
+                    result[2],
+                    result[3],
+                )
+                for result in self._jptemplate.evaluate(base)
+            )
+        except JSONPointerTemplateError as e:
+            raise RelativeJSONPointerTemplateEvaluationError(
+                e.args[0] + f' (after applying {self._relptr}',
+            ) from e
---
+++ tests/test_pointers.py
@@ -0,0 +1,217 @@
+from itertools import chain
+import jschon
+import pytest
+
+from oascomply.pointers import (
+    JSONPointerTemplate,
+    InvalidJSONPointerTemplateError,
+    JSONPointerTemplateEvaluationError,
+    RelativeJSONPointerTemplate,
+    InvalidRelativeJSONPointerTemplateError,
+    RelativeJSONPointerTemplateEvaluationError,
+)
+
+
+@pytest.mark.parametrize('unescaped,escaped', (
+    ('~', '~0'),
+    ('/', '~1'),
+    ('{', '~2'),
+    ('}', '~3'),
+    ('~/{}', '~0~1~2~3'),
+))
+def test_escape(unescaped, escaped):
+    import sys
+    sys.stderr.write(f"{unescaped!r} {escaped!r}")
+    assert JSONPointerTemplate.escape(unescaped) == escaped
+
+
+@pytest.mark.parametrize('escaped,unescaped', (
+    ('~0', '~'),
+    ('~1', '/'),
+    ('~2', '{'),
+    ('~3', '}'),
+    ('~0~1~2~3', '~/{}'),
+))
+def test_unescape(escaped, unescaped):
+    import sys
+    sys.stderr.write(f"{escaped!r} {unescaped!r}")
+    assert JSONPointerTemplate.unescape(escaped) == unescaped
+
+
+@pytest.mark.parametrize('template_str,components', (
+    ('', [jschon.JSONPointer()]),
+    ('/', [jschon.JSONPointer('/')]),
+    ('/foo', [jschon.JSONPointer('/foo')]),
+    ('/~2escaped~3/~1~0', [jschon.JSONPointer('/{escaped}/~1~0')]),
+    ('/{varname}', ['varname']),
+    ('/{varname}#', ['varname', True]),
+    ('/foo/bar/{a}/{b}/qux/quux', [
+        jschon.JSONPointer('/foo/bar'),
+        'a',
+        'b',
+        jschon.JSONPointer('/qux/quux'),
+    ]),
+    ('/bar#', [jschon.JSONPointer('/bar#')]),
+))
+def test_constructor(template_str, components):
+    jpt = JSONPointerTemplate(template_str)
+    assert jpt._components == components
+
+
+@pytest.mark.parametrize('template_str', (
+    '0',  # relative JSON pointer
+    '1#', # relative JSON Pointer
+    '/{no~1escapes}',
+    '/{toosoon}#/etc',
+    '/{trailing}characters',
+    '/leading{characters}',
+))
+def test_constructor_errors(template_str):
+    with pytest.raises(
+        InvalidJSONPointerTemplateError,
+        match='not a valid JSONPointerTemplate',
+    ):
+        JSONPointerTemplate(template_str)
+
+
+TEST_DOCUMENT = jschon.JSON({
+    'a': ['b', 'c', 'd'],
+    'e': {
+        'f': ['g', 'h'],
+        'i': ['j', 'k'],
+    },
+})
+
+
+@pytest.mark.parametrize('template,output', (
+    (
+        JSONPointerTemplate(''),
+        [(jschon.JSONPointer(''), TEST_DOCUMENT, {}, None)],
+    ), (
+        JSONPointerTemplate('/a/0'),
+        [(jschon.JSONPointer('/a/0'), TEST_DOCUMENT['a'][0], {}, None)],
+    ),
+    (
+        JSONPointerTemplate('/e/{key}'),
+        [
+            (
+                jschon.JSONPointer(f'/e/{key}'),
+                TEST_DOCUMENT['e'][key],
+                {'key': key},
+                None,
+            ) for key in TEST_DOCUMENT['e']
+        ],
+    ), (
+        JSONPointerTemplate('/e/{key}#'),
+        [
+            (
+                jschon.JSONPointer(f'/e/{key}'),
+                TEST_DOCUMENT['e'][key],
+                {'key': key},
+                key,
+            ) for key in TEST_DOCUMENT['e']
+        ],
+    ), (
+        JSONPointerTemplate('/e/{key1}/{index}'),
+        list(chain.from_iterable([
+            [
+                (
+                    jschon.JSONPointer(f'/e/{key1}/{index}'),
+                    TEST_DOCUMENT['e'][key1][index],
+                    {'key1': key1, 'index': index},
+                    None,
+                ) for index in range(len(TEST_DOCUMENT['e'][key1]))
+            ] for key1 in TEST_DOCUMENT['e'].keys()
+        ])),
+    ),
+))
+def test_evaluation(template, output):
+    actual = list(
+        template.evaluate(TEST_DOCUMENT)
+    )
+    assert actual == output
+
+
+@pytest.mark.parametrize('template,match', (
+    ('/nope', 'not found in document'),
+    ('/a/0/{scalar}', 'Cannot match template variable'),
+))
+def test_evaluation_errors(template, match):
+    jpt = JSONPointerTemplate(template)
+    with pytest.raises(JSONPointerTemplateEvaluationError, match=match):
+        list(jpt.evaluate(TEST_DOCUMENT))
+
+
+@pytest.mark.parametrize('template_str,relptr,jptemplate', (
+    ('1+3#', jschon.RelativeJSONPointer('1+3#'), None),
+    ('0/foo', jschon.RelativeJSONPointer('0'), JSONPointerTemplate('/foo')),
+    (
+        '1/{bar}#',
+        jschon.RelativeJSONPointer('1'),
+        JSONPointerTemplate('/{bar}#'),
+    ),
+))
+def test_rel_constructor(template_str, relptr, jptemplate):
+    rjpt = RelativeJSONPointerTemplate(template_str)
+    assert rjpt._relptr == relptr
+    assert rjpt._jptemplate == jptemplate
+
+
+@pytest.mark.parametrize('template_str,match', (
+    ('0#/foo', "Can't use '#'"),
+    ('stuff/nonsense', 'not a valid RelativeJSONPointerTemplate'),
+    ('0/{bar}#/foo', 'not a valid RelativeJSONPointerTemplate'),
+))
+def test_rel_constructor_errors(template_str, match):
+    with pytest.raises(InvalidRelativeJSONPointerTemplateError, match=match):
+        RelativeJSONPointerTemplate(template_str)
+
+
+@pytest.mark.parametrize('template,start,output', (
+    (
+        RelativeJSONPointerTemplate('0+1'),
+        jschon.JSONPointer('/a/1').evaluate(TEST_DOCUMENT),
+        [(jschon.RelativeJSONPointer('0+1'), TEST_DOCUMENT['a'][2], {}, None)],
+    ),
+    (
+        RelativeJSONPointerTemplate('1#'),
+        jschon.JSONPointer('/e/f').evaluate(TEST_DOCUMENT),
+        [(jschon.RelativeJSONPointer('1#'), TEST_DOCUMENT['e'], {}, 'e')],
+    ),
+    (
+        RelativeJSONPointerTemplate('0/{var}'),
+        jschon.JSONPointer('/e/i').evaluate(TEST_DOCUMENT),
+        [
+            (
+                jschon.RelativeJSONPointer('0/0'),
+                TEST_DOCUMENT['e']['i'][0],
+                {'var': 0},
+                None,
+            ),
+            (
+                jschon.RelativeJSONPointer('0/1'),
+                TEST_DOCUMENT['e']['i'][1],
+                {'var': 1},
+                None,
+            ),
+        ],
+    ),
+))
+def test_rel_evaluation(template, start, output):
+    actual = list(template.evaluate(start))
+    assert actual == output
+
+@pytest.mark.parametrize('template,start,match', (
+    (
+        RelativeJSONPointerTemplate('1/{whatever}'),
+        TEST_DOCUMENT,
+        'Could not evaluate origin',
+    ), (
+        RelativeJSONPointerTemplate('0/x'),
+        TEST_DOCUMENT,
+        r'Path .* not found .* \(after applying',
+    )
+))
+def test_rel_evaluation_errors(template, start, match):
+    with pytest.raises(RelativeJSONPointerTemplateEvaluationError, match=match):
+        list(template.evaluate(start))
