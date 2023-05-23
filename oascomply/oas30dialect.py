f4674e878028de9b706432e6fce0856eb5b3cdc3
Henry H. Andrews <andrews_henry@yahoo.com>
2023-05-22 22:55:46 -0700

    Add int* and uint* formats
    Also use the decorator to set the type applicability
    for each format, and don't re-check it inside the functions.

--- oascomply/oas30dialect.py
+++ oascomply/oas30dialect.py
@@ -1,6 +1,7 @@
 import re
 import logging
 from collections import defaultdict
+from typing import Union
 
 from jschon import JSON, JSONCompatible, JSONSchema, Result, URI
 from jschon.catalog import Catalog, CatalogError
@@ -356,77 +357,139 @@ class Oas30ExclusiveMinimumKeyword(Keyword):
                 )
 
 
+@format_validator('uint8', instance_types=('number',))
+def validate_uint8(value: Union[int, float]):
+    if value % 1:
+        raise ValueError(f'{value} is not an integer')
+    if value < 0 or value > 255:
+        raise ValueError(f'{value} is outside of uint8 range')
+
+
+@format_validator('uint16', instance_types=('number',))
+def validate_uint16(value: Union[int, float]):
+    if value % 1:
+        raise ValueError(f'{value} is not an integer')
+    if value < 0 or value > 65535:
+        raise ValueError(f'{value} is outside of uint16 range')
+
+
+@format_validator('uint32', instance_types=('number',))
+def validate_uint32(value: Union[int, float]):
+    if value % 1:
+        raise ValueError(f'{value} is not an integer')
+    if value < 0 or value > 4294967295:
+        raise ValueError(f'{value} is outside of uint32 range')
+
+
+@format_validator('uint64', instance_types=('number', 'string'))
+def validate_uint64(value: Union[int, float, str]):
+    if isinstance(value, str):
+        if set(value) - set('0123456789'):
+            raise ValueError("{value!r} is not an integer string")
+        value = int(value)
+    if value % 1:
+        raise ValueError(f'{value} is not an integer')
+    if value < 0 or value > 18446744073709551615:
+        raise ValueError(f'{value} is outside of uint64 range')
+
+
+@format_validator('int8', instance_types=('number',))
+def validate_int8(value: Union[int, float]):
+    if value % 1:
+        raise ValueError(f'{value} is not an integer')
+    if value < -128 or value > 127:
+        raise ValueError(f'{value} is outside of int8 range')
+
+
+@format_validator('int16', instance_types=('number',))
+def validate_int16(value: Union[int, float]):
+    if value % 1:
+        raise ValueError(f'{value} is not an integer')
+    if value < -32768 or value > 32767:
+        raise ValueError(f'{value} is outside of int16 range')
+
+
+@format_validator('int32', instance_types=('number',))
+def validate_int32(value: Union[int, float]):
+    if value % 1:
+        raise ValueError(f'{value} is not an integer')
+    if value < -2147483648 or value > 2147483647:
+        raise ValueError(f'{value} is outside of int32 range')
+
+
+@format_validator('int64', instance_types=('number', 'string'))
+def validate_int64(value: Union[int, float, str]):
+    if isinstance(str):
+        if set(value) - set('0123456789'):
+            raise ValueError("{value!r} is not an integer string")
+        value = int(value)
+    if value % 1:
+        raise ValueError(f'{value} is not an integer')
+    if value < -9223372036854775808 or value > 9223372036854775807:
+        raise ValueError(f'{value} is outside of int64 range')
+
+
 # NOTE: This RFC 3339 implementation does not support "duration"
-@format_validator('date')
+@format_validator('date', instance_types=('string',))
 def validate_relative_json_pointer(value: str) -> None:
-    if isinstance(value, str):
-        # rfc3339 already raises ValueError on error
-        rfc3339.parse_date(value)
+    # rfc3339 already raises ValueError on error
+    rfc3339.parse_date(value)
 
 
-@format_validator('time')
+@format_validator('time', instance_types=('string',))
 def validate_relative_json_pointer(value: str) -> None:
-    if isinstance(value, str):
-        # rfc3339 already raises ValueError on error
-        rfc3339.parse_time(value)
+    # rfc3339 already raises ValueError on error
+    rfc3339.parse_time(value)
 
 
-@format_validator('date-time')
+@format_validator('date-time', instance_types=('string',))
 def validate_relative_json_pointer(value: str) -> None:
-    if isinstance(value, str):
-        # rfc3339 already raises ValueError on error
-        rfc3339.parse_datetime(value)
+    # rfc3339 already raises ValueError on error
+    rfc3339.parse_datetime(value)
 
 
 # NOTE: 'json-pointer' format vaidation included in jschon.formats
-@format_validator('relative-json-pointer')
+@format_validator('relative-json-pointer', instance_types=('string',))
 def validate_relative_json_pointer(value: str) -> None:
-    if isinstance(value, str):
-        if not re.fullmatch(RelativeJSONPointer._regex, value):
-            raise ValueError
+    if not re.fullmatch(RelativeJSONPointer._regex, value):
+        raise ValueError
 
 
-@format_validator('json-pointer-template')
+@format_validator('json-pointer-template', instance_types=('string',))
 def validate_relative_json_pointer(value: str) -> None:
-    if isinstance(value, str):
-        if not re.fullmatch(JSON_POINTER_TEMPLATE, value):
-            raise ValueError
+    if not re.fullmatch(JSON_POINTER_TEMPLATE, value):
+        raise ValueError
 
 
-@format_validator('relative-json-pointer-template')
+@format_validator('relative-json-pointer-template', instance_types=('string',))
 def validate_relative_json_pointer(value: str) -> None:
-    if isinstance(value, str):
-        RelJsonPtrTemplate(value)
-        if not re.fullmatch(RELATIVE_JSON_POINTER_TEMPLATE, value):
-            raise ValueError
+    RelJsonPtrTemplate(value)
+    if not re.fullmatch(RELATIVE_JSON_POINTER_TEMPLATE, value):
+        raise ValueError
 
 
-@format_validator('uri')
+@format_validator('uri', instance_types=('string',))
 def validate_uri(value: str) -> None:
-    if isinstance(value, str):
-        # parse() already raises a ValueError on error
-        rfc3987.parse(value, rule='URI')
+    # parse() already raises a ValueError on error
+    rfc3987.parse(value, rule='URI')
 
 
-@format_validator('uri-reference')
+@format_validator('uri-reference', instance_types=('string',))
 def validate_uri_reference(value: str) -> None:
-    if isinstance(value, str):
-        # parse() already raises a ValueError on error
-        rfc3987.parse(value, rule='URI_reference')
+    # parse() already raises a ValueError on error
+    rfc3987.parse(value, rule='URI_reference')
 
 
-@format_validator('iri')
+@format_validator('iri', instance_types=('string',))
 def validate_iri(value: str) -> None:
-    if isinstance(value, str):
-        # parse() already raises a ValueError on error
-        rfc3987.parse(value, rule='IRI')
+    # parse() already raises a ValueError on error
+    rfc3987.parse(value, rule='IRI')
 
 
-@format_validator('iri-reference')
+@format_validator('iri-reference', instance_types=('string',))
 def validate_iri_reference(value: str) -> None:
-    if isinstance(value, str):
-        # parse() already raises a ValueError on error
-        rfc3987.parse(value, rule='IRI_reference')
+    # parse() already raises a ValueError on error
+    rfc3987.parse(value, rule='IRI_reference')
 
 
 def initialize_oas30_dialect(catalog: Catalog):
@@ -480,15 +543,10 @@ def initialize_oas30_dialect(catalog: Catalog):
     # NOTE: All strings are valid CommonMark, so the "commonmark"
     #       format is not validated.
     catalog.enable_formats(
-        'date',
-        'time',
-        'date-time',
-        'json-pointer',
-        'relative-json-pointer',
-        'json-pointer-template',
-        'relative-json-pointer-template',
-        'uri',
-        'uri-reference',
-        'iri',
-        'iri-reference',
+        'uint8', 'uint16', 'uint32', 'uint64',
+        'int8', 'int16', 'int32', 'int64',
+        'date', 'time', 'date-time',
+        'json-pointer', 'relative-json-pointer',
+        'json-pointer-template', 'relative-json-pointer-template',
+        'uri', 'uri-reference', 'iri', 'iri-reference',
     )
