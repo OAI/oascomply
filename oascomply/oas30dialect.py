304403b50dbb368e9212e3f703e53d88ea9880fc
Henry H. Andrews <andrews_henry@yahoo.com>
2023-05-23 13:58:12 -0700

    Validation tool for schemas using OAS 3.0 dialect
    oas30-schema will load a schema with the OAS 3.0 dialect
    metaschema and assocaited vocabularies, validate the schema
    against the metaschema, and evaluate the instance with the schema.

--- oascomply/oas30dialect.py
+++ oascomply/oas30dialect.py
@@ -550,3 +550,117 @@ def initialize_oas30_dialect(catalog: Catalog):
         'json-pointer-template', 'relative-json-pointer-template',
         'uri', 'uri-reference', 'iri', 'iri-reference',
     )
+
+
+def validate_with_oas30():
+    import sys
+    import argparse
+    import yaml
+    import json
+
+    parser = argparse.ArgumentParser(
+        description='Validates the instance against schemas using the '
+                    'OAS 3.0 Schema Object dialect described by the '
+                    'metaschema "{OAS30_DIALECT_METASCHEMA}"',
+        epilog=f'Note that the schema "{OAS30_DIALECT_METASCHEMA}" is '
+                '*NOT* provided by the OpenAPI Initiative, but is part of the '
+                'oascomply package (oascomply.schemas/oas/v3.0/base.json)',
+    )
+    parser.add_argument(
+        'instance',
+        help='The JSON or YAML file to validate',
+    )
+    parser.add_argument(
+        'schema',
+        help='The schema, in JSON or YAML format, to use',
+    )
+    parser.add_argument(
+        '-r',
+        '--referenced-schema',
+        action='append',
+        dest='refs',
+        default=[],
+        help='NOT YET SUPPORTED! '
+             'An additional schema from which to resolve references; '
+             'can be passed multiple times; note that schema documents '
+             'that reference each other are not currently supported; '
+             'currently, if schema A references schema B, then schema B '
+             'must be passed with -r *BEFORE* schema A',
+    )
+    parser.add_argument(
+        '-o',
+        '--output',
+        choices=('basic', 'detailed', 'verbose'),
+        nargs='?',
+        const='basic',
+        help='On success, print the annotation output to stdout using '
+             'the given standard format; the default annotation format '
+             "is 'basic'",
+    )
+    parser.add_argument(
+        '-e',
+        '--error-format',
+        choices=('basic', 'detailed', 'verbose'),
+        default='detailed',
+        help='Set the output format to use for error reporting; the '
+             "default format is 'detailed'",
+    )
+
+    args = parser.parse_args()
+    metaschema_uri = URI(OAS30_DIALECT_METASCHEMA)
+    metaschema_errors = {}
+
+    # TODO: Actually detect and parse json properly
+    sys.stderr.write(f'Loading instance {args.instance}...\n')
+    with open(args.instance) as inst_fd:
+        instance = JSON(yaml.safe_load(inst_fd))
+
+    # TODO: Be more forgiving about the load order of refschemas,
+    #       as this means that a schema can only a reference another
+    #       schema that has already been loaded
+    for ref in args.refs:
+        # Constructing a JSONSchema registers it with the Catalog
+        sys.stderr.write(f'Loading ref schema {ref}...\n')
+        with open(ref) as ref_fd:
+            ref_schema = JSONSchema(
+                yaml.safe_load(ref_fd),
+                metaschema_uri=metaschema_uri,
+            )
+            meta_result = ref_schema.validate()
+            if not meta_result.valid:
+                metaschema_errors[ref] = meta_result
+
+    sys.stderr.write(f'Loading schema {args.schema}...\n')
+    with open(args.schema) as schema_fd:
+        schema = JSONSchema(
+            yaml.safe_load(schema_fd),
+            metaschema_uri=metaschema_uri,
+        )
+        meta_result = schema.validate()
+        if not meta_result.valid:
+            metaschema_errors[args.schema] = meta_result
+
+    if metaschema_errors:
+        for path, meta_result in metaschema_errors.items():
+            sys.stderr.write(
+                f'OAS 3.0 metaschema validation failed for {args.schema}!\n',
+            )
+            json.dump(
+                meta_result.output(args.error_format),
+                sys.stderr,
+                indent=2,
+            )
+            sys.stderr.write('\n\n')
+        sys.exit(-1)
+
+    sys.stderr.write('Evaluating the instance against the schema...\n')
+    result = schema.evaluate(instance)
+    if result.valid:
+        sys.stderr.write('Your instance is valid!\n')
+        if args.output:
+            json.dump(result.output(args.output), sys.stdout, indent=2)
+            sys.stdout.write('\n')
+    else:
+        sys.stderr.write('Schema validation failed!\n')
+        json.dump(result.output(args.error_format), sys.stderr, indent=2)
+        sys.stderr.write('\n')
